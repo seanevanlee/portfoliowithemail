5ef45f142b863a31438339ff00178bd969c60aad
seanevanlee <seanevanlee@gmail.com>
2023-10-10 00:10:46 -0700

    navbar coming along

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./navbar.css";
 // import logo from "../../assets/logo.png";
+// import contactImg "../../assets/contact.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="{logo}" alt="logo" className="logo" />
-      <div className="desktopMenu"></div>
+      {/* <img src={logo} alt="logo" className="logo" /> */}
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+      </div>
       <button className="desktopMenuBtn">
-        <img src="" alt="" className="desktopMenuImg" />
+        {/* <img src={contactImg} alt="" className="desktopMenuImg" /> */}
         Contact!
       </button>
     </nav>

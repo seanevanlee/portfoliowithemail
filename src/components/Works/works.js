eda3488f28ab4ee8c1d936692c94797d3fc1ebc9
seanevanlee <seanevanlee@gmail.com>
2023-10-11 10:58:51 -0700

    works css

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import "./works.css";
+// import Portfolio1 from '../../assets/portfolio-1.png';
+// import Portfolio2 from '../../assets/portfolio-2.png';
+// import Portfolio3 from '../../assets/portfolio-3.png';
+// import Portfolio4 from '../../assets/portfolio-4.png';
+// import Portfolio5 from '../../assets/portfolio-5.png';
+// import Portfolio6 from '../../assets/portfolio-6.png';
 const works = () => {
-  return <section id="works"></section>;
+  return (
+    <section id="works">
+      <h2 className="workstitle">My Portfolio:</h2>
+      <span>Hope you enjoy my work thus far!</span>
+      <div className="worksImgs">
+        {/* <img src={Portfolio1} alt="" className="worksImg" />
+                <img src={Portfolio2} alt="" className="worksImg" />
+                <img src={Portfolio3} alt="" className="worksImg" />
+                <img src={Portfolio4} alt="" className="worksImg" />
+                <img src={Portfolio5} alt="" className="worksImg" />
+                <img src={Portfolio6} alt="" className="worksImg" /> */}
+      </div>
+    </section>
+  );
 };
 
 export default works;

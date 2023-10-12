e2a183738f428af855dcc627df3d62dfde67c152
seanevanlee <seanevanlee@gmail.com>
2023-10-11 20:49:40 -0700

    footer is live

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from "./components/Intro/intro";
 import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Skills />
       <Works />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return <footer className="footer">Sean Evan Lee </footer>;
+};
+
+export default Footer;

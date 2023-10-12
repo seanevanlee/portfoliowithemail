4bff292ce46f623b0ba5cbd402d6a738b1d1f8f0
seanevanlee <seanevanlee@gmail.com>
2023-10-11 20:57:44 -0700

    navbar active class

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <footer className="footer">Sean Evan Lee </footer>;
+  return <footer className="footer">Copyright &#169; Sean Evan Lee </footer>;
 };
 
 export default Footer;
